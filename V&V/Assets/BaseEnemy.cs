b6e281b014ff3b974a8c65723391ca6afa1d7b08
Saim Zahid <saimzahid@me.com>
2017-10-29 16:41:40 -0400

    Adding in robot sprite assets and base enemy class

---
+++ V&V/Assets/BaseEnemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseEnemy : MonoBehaviour {
+	public float enemyHealth;
+	public object equippedWeapon;
+	public float chanceToDropScrap;
+	public bool doesDropScrap;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void EnemyTakesDamage(float damageAmount) 
+	{
+		enemyHealth -= damageAmount;
+	}
+
+	public void dropScrap()
+	{
+
+	}
+}
---
+++ V&V/Assets/BaseEnemyTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Capstone
+{
+	[TestFixture]
+	public class BaseEnemyTests
+	{
+		BaseEnemy enemy = new BaseEnemy();
+
+
+		[Test]
+		public void EnemyTakesDamage_PositiveAmount_HealthUpdated() 
+		{
+			enemy.enemyHealth = 100f;
+
+			enemy.EnemyTakesDamage (10f);
+
+			Assert.AreEqual (90f, enemy.enemyHealth);
+		}
+	}
+}
+
