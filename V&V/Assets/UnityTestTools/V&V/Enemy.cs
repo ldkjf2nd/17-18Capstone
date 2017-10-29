21fd46e677ce85da6eedda68329e468dfe0aca0a
Saim Zahid <saimzahid@me.com>
2017-10-28 22:28:07 -0400

    dropScrap method added to Enemy class. Empty as of now.

--- V&V/Assets/UnityTestTools/V&V/Enemy.cs
+++ V&V/Assets/UnityTestTools/V&V/Enemy.cs
@@ -5,10 +5,17 @@ namespace Capstone
 	public class Enemy
 	{
 		public float enemyHealth;
+		public bool doesDropScrap;
+		public float chanceToDropScrap;
 
 		public void EnemyTakesDamage(float damageAmount) 
 		{
 			enemyHealth -= damageAmount;
+		}
+
+		public void dropScrap()
+		{
+				
 		}
 	}
 }
