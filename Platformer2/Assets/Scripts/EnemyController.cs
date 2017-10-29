fd2c3f96dca1eca7723bd625b58e73548b679278
thatstoofar <yujd2@mcmaster.ca>
2017-10-29 17:52:51 -0400

    test

---
+++ Platformer2/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+	public Rigidbody2D rb2d; 
+	public PlayerController playercontroller;
+	public float moveForce = 5f; 
+	public int EnemyCount;
+
+
+	// Use this for initialization
+	void Start () {
+
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//enemyTransform.localScale = new Vector3(0f, 0.37f,0f);
+
+		
+	}
+	void Awake () {
+		rb2d = GetComponent<Rigidbody2D> ();
+		EnemyMoveLeft ();
+	}
+	void FixedUpdate(){
+		
+	}
+
+
+	void EnemyMoveLeft(){
+		StartCoroutine (AddLeftForce ());
+
+	}
+	IEnumerator AddLeftForce(){
+		while(true){
+			rb2d.AddForce (Vector2.left * moveForce);
+			yield return new WaitForSeconds (1);
+		}
+	}
+	void OnCollisionEnter2D(Collision2D coll){
+		if (coll.gameObject.CompareTag ("Player") ) {
+			if (PlayerController.grounded) {
+				Application.LoadLevel(Application.loadedLevel);
+			}
+			coll.gameObject.GetComponent <Rigidbody2D> ().AddForce (new Vector2 (0f, PlayerController.jumpForce/2));
+			rb2d.transform.localScale -= new Vector3(0f, 1.85f, 0f);
+			rb2d.gameObject.SetActive (false);
+		}  
+	}
+}
---
+++ Platformer2/Assets/Scripts/EnemySpawnner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnner : MonoBehaviour {
+	Rigidbody2D enemySpawner; 
+	Vector3 spawnEnemyLocation;
+	public GameObject enemy;
+
+	// Use this for initialization
+	void Start () {
+		enemySpawner = GetComponent<Rigidbody2D> ();
+		spawnEnemyLocation = enemySpawner.position;
+			SpawnEndlessEnemies();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		
+	}
+	void SpawnEndlessEnemies(){
+		StartCoroutine (SpawnEnemy ());
+	}
+	IEnumerator SpawnEnemy(){
+		while(true){
+			Instantiate(enemy, spawnEnemyLocation, Quaternion.identity);
+			yield return new WaitForSeconds (5);
+		}
+
+	}
+}
