041e83a8bb386a93cfb6d0224ed18b09e7c2844f
Saim Zahid <saimzahid@me.com>
2017-10-28 22:16:21 -0400

    Tests for Game State

---
+++ V&V/Assets/UnityTestTools/V&V/GameState.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace AssemblyCSharp
+{
+	public class GameState
+	{
+		public bool isANewGame = true;
+
+		public void startGame()
+		{
+			if (isANewGame)
+				//Start a new game
+				//Set isANewGame to false for when user returns to this game state.
+				isANewGame = false;
+			else
+				//Load last save's game state.
+				loadGame();
+		}
+
+		public void loadGame()
+		{
+			//will be implemented later
+		}
+
+		public void saveGame()
+		{
+			//will be implemented later
+		}
+
+
+		public void pauseGame() 
+		{
+			//will be implemented later
+		}
+
+		public void unPauseGame() 
+		{
+			//will be implemented later
+		}
+	}
+}
+
