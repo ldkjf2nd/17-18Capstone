7572a64aa42ca16604c1e7621a30eeb2a7e9066b
Saim Zahid <saimzahid@me.com>
2017-10-30 23:22:02 -0400

    Adding in empty methods for base enemy script.

--- V&V/Assets/BaseEnemy.cs
+++ V&V/Assets/BaseEnemy.cs
@@ -7,20 +7,40 @@ public class BaseEnemy : MonoBehaviour {
 	public object equippedWeapon;
 	public float chanceToDropScrap;
 	public bool doesDropScrap;
-
+  
 	// Use this for initialization
 	void Start () {
-
+		
 	}
 
 	// Update is called once per frame
 	void Update () {
-
 	}
 
 	public void EnemyTakesDamage(float damageAmount) 
 	{
 		enemyHealth -= damageAmount;
+	}
+
+	public void findPlayerOnScreen()
+	{
+		
+	}
+
+	public void useBasicAttack()
+	{
+	}
+
+	public void useSpecialAttack()
+	{
+	}
+
+	public void showSpawnAnimation() 
+	{
+	}
+
+	public void showDeathAnimation()
+	{
 	}
 
 	public void dropScrap()
