7462d13c9e8d90e13db75c27dcb0b9da486a4d20
Jonathan Yu <yujd2@mcmaster.ca>
2018-03-08 22:21:35 -0500

    tests

---
+++ Unity Project/Assets/Resources/Tests/PlayerControllerTest.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class PlayerTest {
+
+	[Test]
+	public void PlayerTestSimplePasses() {
+		// Use the Assert class to test conditions.
+	}
+
+	// A UnityTest behaves like a coroutine in PlayMode
+	// and allows you to yield null to skip a frame in EditMode
+	[UnityTest]
+	public IEnumerator PlayerJumpForceTest() {
+		GameObject gw = GameObject.FindWithTag ("Player");
+		PlayerController gwc = gw.GetComponent<PlayerController> ();
+		Assert.AreEqual(500f, gwc.jumpForce);
+		// Use the Assert class to test conditions.
+		// yield to skip a frame
+		yield return null;
+	}
+
+
+}
